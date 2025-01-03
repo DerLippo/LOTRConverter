6e70ab02189795a543db5401f2c3c8db524056c5
DerLippo <32038450+DerLippo@users.noreply.github.com>
2025-01-03 13:35:39 +0100

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Philipp Oswald on 03.01.25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
